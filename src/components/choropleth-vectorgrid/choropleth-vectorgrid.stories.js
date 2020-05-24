ac9d10f25da85cf168a5d097bce690ea8489c1dd
geomatupen <upendra.oli59@gmail.com>
2020-05-24 23:45:42 +0545

    treemap reactvis added

--- src/components/choropleth-vectorgrid/choropleth-vectorgrid.stories.js
+++ src/components/choropleth-vectorgrid/choropleth-vectorgrid.stories.js
@@ -12,7 +12,8 @@ const options = {
  };
 
 
-
+var colors = ["#fff3d4", "#FED976", "#FEB24C", "#FD8D3C", "#FC4E2A", "#E31A1C", "#BD0026", "#800026"]
+var divisions = [0,5,10,15,20,25,30];
 
 storiesOf('Vectorgrid', module)
   .addDecorator(withKnobs)
@@ -29,6 +30,8 @@ storiesOf('Vectorgrid', module)
             //fillColor : color("Fill Color","#FFFFFF"),
             fill : boolean('Fill', true),
         }}
+        divisions = {[number("division1", divisions[0]),number("division2", divisions[1]),number("division3", divisions[2]),number("division4", divisions[3]),number("division5", divisions[4]),number("division6", divisions[5]),number("division7", divisions[6])]}
+        colorArray = {[color("Color1",colors[0]),color("Color2",colors[1]), color("Color3",colors[2]), color("Color4",colors[3]), color("Color5",colors[4]), color("Color6",colors[5]), color("Color7",colors[6]), color("Color8",colors[7])]}
         onClick={action('click')}
       />
   ))

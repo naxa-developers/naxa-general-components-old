2733a69806d883cc49f4d5f7eac99619f7e5edca
rowheat02 <rohit.jack02@gmail.com>
2020-05-27 15:54:37 +0545

    Pagination

--- src/index.js
+++ src/index.js
@@ -7,5 +7,6 @@ import ProgressBar from './components/ProgressBar';
 import TreeMap from './components/TreeMap';
 import MySankey from './components/Sankey-Nivo';
 import RadarChart from './components/RadarChart';
+import Pagination from './components/Pagination'
 
-export { Button, Badge, PopUp, Antpath, ProgressBar, TreeMap, MySankey, RadarChart };
+export { Button, Badge, PopUp, Antpath, ProgressBar, TreeMap, MySankey, RadarChart, Pagination };
